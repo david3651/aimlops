cdfb86bd82adcad1ebff2bb9a1832330da60e6a6
david3651 <david3651@live.com>
2025-04-22 02:14:22 +0000

    flake8

--- src/model/train.py
+++ src/model/train.py
@@ -53,7 +53,7 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # Train model
-    model = LogisticRegression(C=1/reg_rate, solver="liblinear")
+    model = LogisticRegression(C=1 / reg_rate, solver="liblinear")
     model.fit(X_train, y_train)
 
     # Evaluate model
